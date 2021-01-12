37d3ec0deb25e92f9f8f84b853af66505a98ca7d
Joao Pinelo <j.pinelo@mac.com>
2021-01-12 07:13:28 -0100

    cleanup

--- Session1/1.R
+++ Session1/1.R
@@ -62,7 +62,7 @@ IQR(a)
 
 # PARAMETRIC VS NON-PARAMETRIC
 
-# PARAMETRIC ASSUMES DATA FOLLOW A DISTRIBUTION (usually normal)
+# PARAMETRIC ASSUMES DATA FOLLOWS A DISTRIBUTION (usually normal)
 
 # Perason's corr coef. - Parametric
 # only for nomral distrib AND linear dependence 
